79fee5fe7301256ba284041c09f352ec884dc552
josh <joshua@marinacci.org>
2021-09-16 17:54:58 -0700

    work on pure js version of the test

--- keylang/lib.js
+++ keylang/lib.js
@@ -10,13 +10,156 @@ export class KList {
         })
         this.get = (index)=>this.data[index]
     }
+    get length() {
+        return this.data.length
+    }
     // get(index) {
     //     console.log(`KList.get(${index})`)
     //     return this.data[index]
     // }
+    map(cb) {
+        return new KList(this.data.map(cb))
+    }
+    forEach(cb) {
+        // console.log("calling for each on",this.data)
+        this.data.forEach(cb)
+    }
 }
 
+export class KCanvas {
+    constructor(x,y,w,h,selector) {
+        this.canvas = document.querySelector(selector)
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.scale = 10
+    }
+    get width() {
+        return this.w
+    }
+    get size() {
+        return [this.w,this.h]
+    }
+    setPixel(xy,color) {
+        let ctx = this.canvas.getContext('2d')
+        let x = xy.get(0)
+        let y = xy.get(1)
+        // console.log("drawing at",x,y,color)
+        ctx.fillStyle = color.toCSSColor()
+        // console.log("fillstyle is",color.toCSSColor())
+        ctx.fillRect(
+            Math.floor(x)*this.scale,
+            Math.floor(y)*this.scale,
+            this.scale,
+            this.scale,
+        )
+    }
 
+    clear() {
+        let ctx = this.canvas.getContext('2d')
+        ctx.fillStyle = 'black'
+        ctx.fillRect(0,0,this.canvas.width,this.canvas.height)
+    }
+}
+
+export class KColor {
+    constructor(r,g,b) {
+        this.r = r
+        this.g = g
+        this.b = b
+    }
+    toCSSColor() {
+        return `rgb(${Math.floor(this.r*255)}, ${Math.floor(this.g*255)},${Math.floor(this.b*255)})`
+    }
+}
+export class KObj {
+    constructor() {
+    }
+}
+export class KPoint {
+    constructor(x,y) {
+        this.data = [x,y]
+    }
+}
+export class KVector {
+    constructor(x,y) {
+        this.data = [x,y]
+    }
+}
+export class KRect {
+    constructor(x,y,w,h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+    }
+}
+
+export function add(a,b) {
+    // console.log('adding',a,b)
+    if(a.data && b.data) {
+        let new_data = a.data.map((aa,i) => {
+            return aa + b.data[i]
+        })
+        return new KList(new_data)
+    }
+    return a + b
+}
+export function choose(list) {
+    let n = Math.floor(Math.random()*list.length)
+    return list.get(n)
+}
+export function randi(min,max) {
+    if(typeof max === 'undefined') return randi(0,min)
+    return Math.floor(Math.random()*(max-min) + min)
+}
+export function randf(min,max) {
+    if(typeof max === 'undefined')  return randf(0,min)
+    return Math.random()*(max-min) + min
+}
+export function range(...args) {
+    let arr = []
+    for(let i=0; i<args[0]; i++) {
+        arr[i]= i
+    }
+    return new KList(arr)
+}
+export function wait(time) {
+    // console.log('waiting for time',time)
+}
+
+function get_at(arr,i) {
+    if(arr.data) return arr.get(i)
+    return arr[i]
+}
+function triple_map(a,b,c,cb) {
+    let res = []
+    for(let i=0; i<a.length; i++) {
+        let aa = get_at(a,i)
+        let bb = get_at(b,i)
+        let cc = get_at(c,i)
+        res.push(cb(aa,bb,cc))
+    }
+    return new KList(res)
+}
+
+export function wrap(val, min, max) {
+    // console.log('wrapping',val,'between',min,'and',max)
+    if(val.data ) {
+        // console.log("wrapping a list")
+        return triple_map(val,min,max,(a,b,c)=>{
+            return wrap(a,b,c)
+        })
+        // return new KList([
+        //     wrap(val.get(0),min.get(0),max.get(0)),
+        //     wrap(val.get(1),min.get(1),max.get(1))
+        // ])
+    }
+    if(val < min) return val + (max-min)
+    if(val > max) return val - (max-min)
+    return val
+}
 export const STD_SCOPE = {
     List:(...args)=>{
         // console.log("got the args",...args)
---
+++ keylang/test1.js
@@ -0,0 +1,42 @@
+import {KCanvas, KList, KRect, KColor, KObj,KPoint, KVector,randi,randf,choose,range,wrap,add, wait} from "./lib.js"
+
+let screen = new KCanvas(0,0,64,32,"#canvas")
+/* ========== everything above here will be generated */
+
+let palette = new KList()
+let black = new KColor(0,0,0)
+let red = new KColor(1,0,0)
+let green =new KColor(0,1,0)
+let blue = new KColor(0,0,1)
+
+let dots
+function setup_drips() {
+    palette = new KList(black,red,green,blue)
+    function MakeDot() {
+        let dot = new KObj()
+        dot.xy = new KPoint(randi(screen.width),randi(1))
+        dot.v = new KVector(randf(0,1), randf(0.5,1.5))
+        dot.color = choose(palette)
+        return dot
+    }
+    dots = range(20).map(()=>MakeDot())
+
+}
+
+function draw_dots() {
+    dots.forEach(dot => {
+        dot.xy = wrap(add(dot.xy,dot.v), [0,0], screen.size)
+        screen.setPixel(dot.xy,dot.color)
+    })
+    wait(1)
+}
+
+/* ========== everything below here will be generated ========= */
+
+setup_drips()
+function do_cycle() {
+    screen.clear()
+    draw_dots()
+    setTimeout(do_cycle,100)
+}
+do_cycle()
