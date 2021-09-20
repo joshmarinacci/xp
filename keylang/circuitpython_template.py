81f2e0ba398894c69fd7524d7a6ea5b336899636
josh <joshua@marinacci.org>
2021-09-19 18:02:39 -0700

    disable the python timeout

--- keylang/circuitpython_template.py
+++ keylang/circuitpython_template.py
@@ -54,8 +54,8 @@ _SYSTEM_running = True
 while _SYSTEM_running:
     tm.cycle(0.01)
     now = time.monotonic()
-    if now > start_time + 20:
-        _SYSTEM_running = False
+#     if now > start_time + 20:
+#         _SYSTEM_running = False
 
 
 print("end everything")
