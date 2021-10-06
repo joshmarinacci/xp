c3c188f43f6ef2fde235ee8a2f3233191f6cfc93
josh <joshua@marinacci.org>
2021-10-05 17:58:51 -0700

    more work on compiler

--- keylang/process.js
+++ keylang/process.js
@@ -1,11 +1,43 @@
-function compile_artlang(input, output) {
+import child_process from 'child_process'
+import {promisify} from 'util'
+import path from 'path'
+import fs from 'fs'
+
+async function compile_artlang(input, output) {
     debug('compiling artlang',input,'to',output)
 }
-function copy_files(input, output) {
+async function copy_files(input, output) {
     debug('copying',input,'to',output)
 }
-function compile_mpy(input, output) {
-    debug("compiling mpy",input,'to',output)
+
+const execFile = promisify(child_process.execFile)
+async function compile_lib(file) {
+    const bin ="/Users/josh/projects/xp/keylang"+'/mpy-cross-macos-universal-7.0.0-108-g39886701d.app'
+    let dir = path.dirname(file)
+    let args = [file]
+    debug("compile_lib",bin,args)
+    let res = await execFile(bin, args, {
+        cwd:'/Users/josh/projects/xp/keylang'
+    })
+    console.log("results are",res)
+    let mpyname = path.basename(file,'.py') + '.mpy'
+    return path.join(dir,mpyname)
+}
+
+async function copy_file_to_dir(lib_file, out_dir) {
+    let basename = path.basename(lib_file)
+    let outfile = path.join(out_dir,basename)
+    debug("copy file from",lib_file, 'to', outfile)
+    await fs.promises.copyFile(lib_file, outfile)
+}
+
+async function compile_mpy(input, out_dir) {
+    debug("compiling mpy",input,'to',out_dir)
+    for(let file of input) {
+        let lib_file = await compile_lib(file)
+        debug("lib file is",lib_file)
+        await copy_file_to_dir(lib_file, out_dir)
+    }
 }
 
 const targets = {
@@ -18,11 +50,11 @@ const targets = {
         process: [compile_artlang],
     },
     '@matrix': {
-        inputs:['py_libs/tasks.py',
-            'py_libs/matrix.py',
-            'py_libs/common.py'],
+        inputs:['libs_py/tasks.py',
+            'libs_py/matrix.py',
+            'libs_py/common.py'],
         output:'/Volumes/CIRCUITPY/',
-        process:[compile_mpy, copy_files],
+        process:[compile_mpy],
     },
 }
 
@@ -55,4 +87,4 @@ function make_target(targets, target, opts) {
     // debug(info)
 }
 
-make_target(targets,'project',{debug:true})
+make_target(targets,'@matrix',{debug:true})
